fc3a1bce30f16b03c8802230eae7a4281e1e7f1f
Aya Bouguessa <abougues@c3r5p2.1337.ma>
2026-04-25 18:21:05 +0100

    caca

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-import llm
+import os
+os.environ["HF_HOME"] = "/goinfre/abougues/hf-cache"
+
+from llm_sdk import Small_LLM_Model
+
+
+def main():
+    '''idk yet???'''
+
+    # AiAi = Small_LLM_Model()
+    print("here")
+    lista = [1242,  315,  220,   19,   17,  323,  481,   17]
+
+    print(huuh)
+
+
+print("caca")
+main()
